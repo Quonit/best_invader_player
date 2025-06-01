66180db4911b70b3982753477e661195917fb50a
Quonit <63931758+Quonit@users.noreply.github.com>
2025-05-31 20:48:20 -0700

    idk what im doing I'm so sorry discord attachments

--- quos_music_player_css.c
+++ quos_music_player_css.c
@@ -1,6 +1,6 @@
 #player,#player a{ color:#000000 ;  font-size: 11px; font-family: verdana;}
-#player{ background:url(https://media.discordapp.net/attachments/718156205645758504/1144453930068213760/image.png?width=288&height=32); background-position:bottom; background-color: #000000;}
-#player .mainImage{ background:url(https://cdn.discordapp.com/attachments/971640812012183583/1144453206634672278/big_tile_thing.png); }
+#player{ background:url(https://i.postimg.cc/1Rrsb7qm/big-tile-thing.png); background-position:bottom; background-color: #000000;}
+#player .mainImage{ background:url(https://i.postimg.cc/1Rrsb7qm/big-tile-thing.png; }
 
 #player #baseLeft{ width:93px; } 
 #player #baseRight{ width:145px;} 
@@ -24,7 +24,7 @@
 
 #playlist,#playlist .module,#playlist h3{ border-color:#000000;}
 #playlist .module{  color:#000000; background-color:#000000; }
-#playlist h3{ background:url(https://cdn.discordapp.com/attachments/718156205645758504/1144455232080842752/image.png); color:#000000; background-color:#000000;  }
+#playlist h3{ background:url(https://i.postimg.cc/1Rrsb7qm/big-tile-thing.png); color:#000000; background-color:#000000;  }
 #playlist .even{ background-color:#fff;}
 #playlist .odd{ background-color:#fff;}
 #playlist .focus{ background-color:#fff; color:#aaa;}   
