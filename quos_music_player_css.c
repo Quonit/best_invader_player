c22c966f217c63e574c28def2cd670d71e33cb68
Quonit <63931758+Quonit@users.noreply.github.com>
2025-05-31 20:39:42 -0700

    plsplsplspls give me music player back

---
+++ quos_music_player_css.c
@@ -0,0 +1,30 @@
+#player,#player a{ color:#000000 ;  font-size: 11px; font-family: verdana;}
+#player{ background:url(https://media.discordapp.net/attachments/718156205645758504/1144453930068213760/image.png?width=288&height=32); background-position:bottom; background-color: #000000;}
+#player .mainImage{ background:url(https://cdn.discordapp.com/attachments/971640812012183583/1144453206634672278/big_tile_thing.png); }
+
+#player #baseLeft{ width:93px; } 
+#player #baseRight{ width:145px;} 
+#player .button{  width:26px; }
+#player #play{ background:url(https://media.discordapp.net/attachments/718156205645758504/1144454339906240662/image.png); }
+#player #play,#player #pause{ left:26px; }
+#player #previous{ left:0px; }
+#player #next{ left:52px; }
+#player #title{ left:90px;  font-size: 9px; }
+#player #tooltip{ right:196px; }    
+#player #timer{ right:120px;  } 
+#player #list{ right:0px; } 
+
+#player #seeker{ right:320px; }
+#player #seekerBase{ border-color:#000;  }
+#player #seekerBuffered{ background-color:#000000 ; }
+#player #seekerSliderBase{ background-color:#000; } 
+
+#player #volume{ width:65px; right:39px; } 
+#player #volumeSlider{ width:12px; background:url(http://static.tumblr.com/su8juwr/z61mqkq8i/images__5_-crop.jpg); }
+
+#playlist,#playlist .module,#playlist h3{ border-color:#000000;}
+#playlist .module{  color:#000000; background-color:#000000; }
+#playlist h3{ background:url(https://cdn.discordapp.com/attachments/718156205645758504/1144455232080842752/image.png); color:#000000; background-color:#000000;  }
+#playlist .even{ background-color:#fff;}
+#playlist .odd{ background-color:#fff;}
+#playlist .focus{ background-color:#fff; color:#aaa;}   
